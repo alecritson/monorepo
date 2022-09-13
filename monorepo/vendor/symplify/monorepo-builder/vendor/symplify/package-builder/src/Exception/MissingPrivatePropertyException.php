83c8cb7a5eec1aaaa39ae17bca5a2d6978437f7c
Alec Ritson <hello@itsalec.co.uk>
2022-09-13 14:33:34 +0100

    whoops

--- monorepo/vendor/symplify/monorepo-builder/vendor/symplify/package-builder/src/Exception/MissingPrivatePropertyException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace MonorepoBuilder202209\Symplify\PackageBuilder\Exception;
-
-use Exception;
-final class MissingPrivatePropertyException extends Exception
-{
-}
--- monorepo/vendor/symplify/monorepo-builder/vendor/symplify/symplify-kernel/src/Exception/BootException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace MonorepoBuilder202209\Symplify\SymplifyKernel\Exception;
-
-use Exception;
-final class BootException extends Exception
-{
-}
